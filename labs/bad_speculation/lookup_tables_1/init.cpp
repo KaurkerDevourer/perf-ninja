2a443bed272e8e9879498c829c0efc3e54296e03
Stepan Styopkin <smstyopkin@edu.hse.ru>
2025-02-05 13:08:52 +0000

    something

--- labs/bad_speculation/lookup_tables_1/init.cpp
+++ labs/bad_speculation/lookup_tables_1/init.cpp
@@ -2,6 +2,18 @@
 #include <limits>
 #include <random>
 
+std::size_t initToBucket(std::size_t v) {
+                                //   size of a bucket
+    if      (v < 13)  return 0; //   13
+    else if (v < 29)  return 1; //   16
+    else if (v < 41)  return 2; //   12
+    else if (v < 53)  return 3; //   12
+    else if (v < 71)  return 4; //   18
+    else if (v < 83)  return 5; //   12
+    else if (v < 100) return 6; //   17
+    return DEFAULT_BUCKET;
+}
+
 void init(std::vector<int> &values) {
   std::random_device r;
   std::default_random_engine generator(r());
--- labs/bad_speculation/lookup_tables_1/solution.cpp
+++ labs/bad_speculation/lookup_tables_1/solution.cpp
@@ -1,16 +1,28 @@
 #include "solution.hpp"
 
-static std::size_t mapToBucket(std::size_t v) {
-                              //   size of a bucket
-  if      (v < 13)  return 0; //   13
-  else if (v < 29)  return 1; //   16
-  else if (v < 41)  return 2; //   12
-  else if (v < 53)  return 3; //   12
-  else if (v < 71)  return 4; //   18
-  else if (v < 83)  return 5; //   12
-  else if (v < 100) return 6; //   17
-  return DEFAULT_BUCKET;
-}
+constexpr std::size_t DATA[151] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 
+  4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 
+  7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7};
+
+#ifdef SOLUTION
+  static std::size_t mapToBucket(std::size_t v) {
+    return DATA[v];
+  }
+#else
+  static std::size_t mapToBucket(std::size_t v) {
+                                //   size of a bucket
+    if      (v < 13)  return 0; //   13
+    else if (v < 29)  return 1; //   16
+    else if (v < 41)  return 2; //   12
+    else if (v < 53)  return 3; //   12
+    else if (v < 71)  return 4; //   18
+    else if (v < 83)  return 5; //   12
+    else if (v < 100) return 6; //   17
+    return DEFAULT_BUCKET;
+  }
+#endif
+
+
 
 std::array<std::size_t, NUM_BUCKETS> histogram(const std::vector<int> &values) {
   std::array<std::size_t, NUM_BUCKETS> retBuckets{0};
